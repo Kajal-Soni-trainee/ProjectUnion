305418e33b55572528cb9c3e3b20826959322572
Kajal-Soni-Trainee <kajal.soni.2024.traine@gmail.com>
2024-03-28 18:55:20 +0530

    Component grid

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const session = require("express-session");
 app.use(session({resave:false, saveUninitialized:true, secret:"your secret key"}));
 app.use(cookieParser());
 const SECRET_KEY="Kajal123";
-//const functions =require("./projects/component_grid/middleware.js");
-//const functions_df= require('./projects/dynamic_form_creation/middleware.js');
+const functions =require("./Projects/component_grid/middleware.js");
+//const functions_df= require('./Projects/dynamic_form_creation/middleware.js');
 const {PORT} = process.env;
 console.log(PORT);
 app.set("view engine", "ejs");
@@ -486,6 +486,152 @@ app.get("/simple_crud_delete",checkSession,(req,res)=>{
   }  
 });
 
+
+// dynamic component grid
+
+var isPagination=false;
+var isFilter=false;
+var idSelected=false;
+app.get("/grid",(req,res)=>{
+   
+    res.render("component_grid_view/index",{isPagi:false, showData:false,result:"",pagination:"pagination",tables:"tables",id:1, limit:0,pages:0,query:"",order:"",element:"",filter:false,obj:""});
+    res.end();
+    
+});
+
+app.post("/grid",(req,res)=>{
+    if(!isFilter){
+        isFilter=true;
+    console.log(req.method);
+    console.log(req.body);
+ var query = req.body.query;
+  let myquery=query;
+   let sql_result;
+ console.log(query);
+ 
+ functions.sql_function(query,conn).then((result)=>{
+     //res.render("index");
+      let len = result.length;
+      let keys = Object.keys(result[0]);
+      let element=keys[0];
+      let order="asc";
+      //console.log(element);
+      let max=20;
+    if(len<max){
+        functions.sql_Orderby(query,element,order,conn).then((result)=>{
+            res.render("component_grid_view/index",{isPagi:false, showData:true,result:result,pagination:"pagination",tables:"tables",id:1, limit:0,pages:0,query:query,order:order,element:element,filter:false,obj:""});
+            res.end();
+        }).catch((err)=>{
+            console.log(err);
+        });
+      
+    }
+     else{
+         isPagination=true;
+        let pages=Math.ceil(len/max);
+        functions.sql_limit(query,1,max,element,order,conn).then((result)=>{
+            res.render("component_grid_view/index",{isPagi:true, showData:true,result:result,pagination:"pagination",tables:"tables", id:1, limit:max,pages:pages,query:query,order:order,element:element,filter:false,obj:""});
+            res.end();
+        }).catch((err)=>{
+            console.log(err);
+        });
+    
+     }
+ }).catch((err)=>{
+    console.log(err);
+ });
+}
+else{
+      
+        console.log(req.body);
+       // let keys = Object.keys(req.body); 
+        let query=req.body.query;
+        let element=req.body.element;
+        let order=req.body.order;
+        //let values=Object.values(req.body);
+       
+      const obj=[];
+      obj.push(req.body);
+      console.log(obj);
+       functions.sql_filters(query,conn,element,order,obj).then((result)=>{
+       // console.log(result);
+        let len =result.length;
+    
+       let limit = 20;
+        if(len<limit){
+            console.log(obj);
+         res.render("component_grid_view/index",{isPagi:false, showData:true,result:result,pagination:"pagination",tables:"tables",id:1, limit:0,pages:0,query:query,order:order,element:element,filter:true,obj:obj});
+         res.end();
+        }
+        else{
+            pages=Math.ceil(len/limit);
+            functions.sql_filter_limit(query,1,limit,element,order,conn,obj).then((result)=>{
+                console.log(obj);
+                res.render("component_grid_view/index",{isPagi:true, showData:true,result:result,pagination:"pagination",tables:"tables", id:1, limit:limit,pages:pages,query:query,order:order,element:element,filter:true,obj:obj});
+                res.end();
+            }).catch((err)=>{
+                console.log(err);
+            });
+        }
+        }).catch((err)=>{
+            console.log(err);
+        }); 
+        isFilter=false;
+        
+    }
+
+});
+
+
+app.get('/data',(req,res)=>{
+    let id=parseInt(req.query.id);
+    let query=req.query.query;
+    let limit=req.query.limit;
+    let pages=req.query.pages;
+    let order=req.query.order;
+    let element=req.query.element;
+    functions.sql_limit(query,id,limit, element,order,conn).then((result)=>{
+        res.render("component_grid_view/index",{isPagi:true, showData:true,result:result,pagination:"pagination",tables:"tables", id:id, limit:limit,pages:pages,query:query,order:order,element:element,filter:false,obj:""});
+        res.end();
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
+
+app.post('/id',(req,res)=>{
+ let query=req.body.query;
+ let id = Object.keys(req.body)[0];
+ let value=Object.values(req.body)[0];
+ functions.sql_id(query,id,value,conn).then((result)=>{
+    res.render("component_grid_view/index",{isPagi:false, showData:true,result:result,pagination:"pagination",tables:"tables",id:1, limit:0,pages:0,query:query,order:"asc",element:id,filter:true,obj:""});
+    res.end();
+ }).catch((err)=>{
+    console.log(err);
+ });
+});
+
+app.get('/filter_data',(req,res)=>{
+     let id= (req.query.id);
+     let limit=req.query.limit;
+     let query=req.query.query;
+     let element=req.query.element;
+     let order=req.query.order;
+     let pages =req.query.pages;
+     let obj=req.query.obj;
+     let result=req.query.result;
+    //  console.log(query);
+    //  console.log(element);
+    //  console.log(order);
+   //console.log(obj);
+   // console.log(query);
+   
+     functions.sql_filter_limit(query,1,limit,element,order,conn,obj).then((result)=>{
+        res.render("component_grid_view/index",{isPagi:true, showData:true,result:result,pagination:"pagination",tables:"tables", id:id, limit:limit,pages:pages,query:query,order:order,element:element,filter:true,obj:obj});
+        res.end();
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
 
 
 
