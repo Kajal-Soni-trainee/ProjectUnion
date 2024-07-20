2f7054b108bb9c824121e24eb1d292912bf7d5b1
kajal.soni <kajal.soni.2024.traine@gmail.com>
2024-07-20 17:27:12 +0530

    rectified code

--- controllers/controllerLoginSystem.js
+++ controllers/controllerLoginSystem.js
@@ -1,146 +1,220 @@
-const { insertUser, findById, updatePassword, findByValue, deleteById, findAll } = require('../models/sql_function');
-const md5 = require('md5');
+const {
+  insertUser,
+  findById,
+  updatePassword,
+  findByValue,
+  deleteById,
+  findAll,
+} = require("../models/sql_function");
+const md5 = require("md5");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const SECRET_KEY = "Kajal123";
 const openLoginSystem = (req, res) => {
-    res.render('login_system_views/index', { errMsg: "", id: 0, otp: 0, isReg: false });
-    res.end();
-}
+  res.render("login_system_views/index", {
+    errMsg: "",
+    id: 0,
+    otp: 0,
+    isReg: false,
+  });
+  res.end();
+};
 
 const registerUser = async (req, res) => {
-    console.log("here its server");
-    console.log(req.body);
-    await insertUser(obj);
-}
+  console.log("here its server");
+  console.log(req.body);
+  const [errMsg, id, otp, isReg] = await insertUser(req.body);
+
+  res.render("login_system_views/index", {
+    errMsg: errMsg,
+    id: id,
+    otp: otp,
+    isReg: isReg,
+  });
+};
 
 const openPasswordPage = (req, res) => {
-    let id = req.query.id;
-    let otp = req.query.otp;
-    res.render('login_system_views/password', { id: id, otp: otp, isSuccess: false, errMsg: "" });
-    res.end();
-}
+  console.log("result ", req.query);
+  let id = req.query.id;
+  let otp = req.query.otp;
+
+  res.render("login_system_views/password", {
+    id: id,
+    otp: otp,
+    isSuccess: false,
+    errMsg: "",
+  });
+  res.end();
+};
 const createPassword = async (req, res) => {
-    let id = req.body.id;
-    let pass = req.body.upass;
-    let u_otp = req.body.otp;
-    console.log(u_otp);
-    let result = await findById('users', 'user_id', id);
-    if (result.length == 0) {
-        res.render('login_system_views/password', { id: 0, otp: u_otp, isSuccess: false, errMsg: "user does not exist please register first" });
-        res.end();
-    }
-    else {
-        let password = md5(result[0].u_salt + pass);
-        let result = await updatePassword(password, id, u_otp)
-        if (result.length != 0) {
-            console.log("update successfully");
-            res.render('login_system_views/password', { id: 0, otp: u_otp, isSuccess: true, errMsg: "" });
-            res.end();
-        }
-    }
-}
-const openLoginPage = (req, res) => {
-    res.render('login_system_views/login', { email: "", errMsg: "", successMsg: "" });
+  let id = req.body.id;
+  let pass = req.body.upass;
+  let u_otp = req.body.otp;
+  console.log(u_otp);
+  let result = await findById("users", "user_id", id);
+  if (result.length == 0) {
+    res.render("login_system_views/password", {
+      id: 0,
+      otp: u_otp,
+      isSuccess: false,
+      errMsg: "user does not exist please register first",
+    });
     res.end();
-}
+  } else {
+    let password = md5(result[0].u_salt + pass);
+    let resultPass = await updatePassword(password, id, u_otp);
+    if (resultPass.length != 0) {
+      console.log("update successfully");
+      res.render("login_system_views/password", {
+        id: 0,
+        otp: u_otp,
+        isSuccess: true,
+        errMsg: "",
+      });
+      res.end();
+    }
+  }
+};
+const openLoginPage = (req, res) => {
+  res.render("login_system_views/login", {
+    email: "",
+    errMsg: "",
+    successMsg: "",
+  });
+  res.end();
+};
 
 const loginUser = async (req, res) => {
-    let email = req.body.email;
-    let pass = req.body.pass;
-    let result = await findByValue('users', 'u_email', email);
-    if (result.length == 0) {
-        let errMsg = 'invalid username or password';
-        res.render('login_system_views/login', { email: email, errMsg: errMsg, successMsg: "" });
-        res.end();
+  let email = req.body.email;
+  let pass = req.body.pass;
+  let result = await findByValue("users", "u_email", email);
+  if (result.length == 0) {
+    let errMsg = "invalid username or password";
+    res.render("login_system_views/login", {
+      email: email,
+      errMsg: errMsg,
+      successMsg: "",
+    });
+    res.end();
+  } else {
+    let dbpass = result[0].u_pass;
+    let salt = result[0].u_salt;
+    let u_email = result[0].u_email;
+    let u_name = result[0].u_name;
+    let newPass = md5(salt + pass);
+    if (dbpass != newPass) {
+      let errMsg = "invalid username or password";
+      res.render("login_system_views/login", {
+        email: email,
+        errMsg: errMsg,
+        successMsg: "",
+      });
+      res.end();
+    } else {
+      let successMsg = "Logged in Successfully";
+      const token = jwt.sign({ email: u_email }, SECRET_KEY);
+      res.cookie("token", token, {
+        maxAge: 1000 * 60 * 60 * 2,
+        httpOnly: true,
+      });
+      res.render("login_system_views/home", { u_name: u_name });
+      res.end();
     }
-    else {
-        let dbpass = result[0].u_pass;
-        let salt = result[0].u_salt;
-        let u_email = result[0].u_email;
-        let u_name = result[0].u_name;
-        let newPass = md5(salt + pass);
-        if (dbpass != newPass) {
-            let errMsg = 'invalid username or password';
-            res.render('login_system_views/login', { email: email, errMsg: errMsg, successMsg: "" });
-            res.end();
-        }
-        else {
-            let successMsg = "Logged in Successfully";
-            const token = jwt.sign({ email: u_email }, SECRET_KEY);
-            res.cookie('token', token, {
-                maxAge: 1000 * 60 * 60 * 2,
-                httpOnly: true,
-            });
-            res.render('login_system_views/home', { u_name: u_name });
-            res.end();
-        }
-    }
-}
+  }
+};
 
 const checkdb = async (req, res) => {
-    let id = req.query.id;
-    let result = await findById('users', 'user_id', id);
-    if (result[0].u_pass == null) {
-        let result1 = await deleteById('users', 'user_id', id);
-        if (result1.length != 0) {
-            console.log("successfully deleted");
-        }
+  let id = req.query.id;
+  let result = await findById("users", "user_id", id);
+  if (result[0].u_pass == null) {
+    let result1 = await deleteById("users", "user_id", id);
+    if (result1.length != 0) {
+      console.log("successfully deleted");
     }
-
-}
+  }
+};
 
 const forgetPass = async (req, res) => {
-    let email = req.query.email;
-    if (email == "") {
-        let errMsg = "please try first";
-        res.render('login_system_views/login', { email: "", errMsg: errMsg, successMsg: "" });
+  let email = req.query.email;
+  if (email == "") {
+    let errMsg = "please try first";
+    res.render("login_system_views/login", {
+      email: "",
+      errMsg: errMsg,
+      successMsg: "",
+    });
+    res.end();
+  } else {
+    let result = await findByValue("users", "u_email", email);
+    if (result.length != 0) {
+      let id = result[0].user_id;
+      let otp = result[0].u_otp;
+      if (result[0].u_pass != null) {
+        res.render("login_system_views/password", {
+          id: id,
+          otp: otp,
+          isSuccess: false,
+          errMsg: "",
+        });
         res.end();
+      }
+    } else {
+      let errMsg = "Invalid credentials";
+      res.render("login_system_views/login", {
+        email: "",
+        errMsg: errMsg,
+        successMsg: "",
+      });
+      res.end();
     }
-    else {
-        let result = await findByValue('users', 'u_email', email)
-        if (result.length != 0) {
-            let id = result[0].user_id;
-            let otp = result[0].u_otp;
-            if (result[0].u_pass != null) {
-                res.render('login_system_views/password', { id: id, otp: otp, isSuccess: false, errMsg: "" });
-                res.end();
-            }
-        }
-        else {
-            let errMsg = "Invalid credentials";
-            res.render('login_system_views/login', { email: "", errMsg: errMsg, successMsg: "" });
-            res.end();
-        }
-    }
-}
+  }
+};
 
 const displayList = async (req, res) => {
-    let token = req.headers.cookie.split("=")[1];
-    if (token) {
-        console.log(token);
-        let result = await findAll('users');
-        if (result.length != 0) {
-            res.render('login_system_views/list', { result: result });
-            res.end();
-        }
+  let token = req.headers.cookie.split("=")[1];
+  if (token) {
+    console.log(token);
+    let result = await findAll("users");
+    if (result.length != 0) {
+      res.render("login_system_views/list", { result: result });
+      res.end();
     }
-    else {
-        let errMsg = "You need to login first";
-        res.render('login_system_views/login', { email: "", errMsg: errMsg, successMsg: "" });
-        res.end();
-    }
-}
+  } else {
+    let errMsg = "You need to login first";
+    res.render("login_system_views/login", {
+      email: "",
+      errMsg: errMsg,
+      successMsg: "",
+    });
+    res.end();
+  }
+};
 
 const openHomePage = (req, res) => {
-    res.render('home');
-    res.end();
-}
+  res.render("home");
+  res.end();
+};
 
 const logoutUser = (req, res) => {
-    res.clearCookie("token");
-    res.render('login_system_views/login', { email: "", errMsg: "", successMsg: "" });
-    res.end();
-}
-module.exports = { openLoginSystem, registerUser, openPasswordPage, createPassword, openLoginPage, loginUser, checkdb, forgetPass, displayList, openHomePage, logoutUser };
+  res.clearCookie("token");
+  res.render("login_system_views/login", {
+    email: "",
+    errMsg: "",
+    successMsg: "",
+  });
+  res.end();
+};
+module.exports = {
+  openLoginSystem,
+  registerUser,
+  openPasswordPage,
+  createPassword,
+  openLoginPage,
+  loginUser,
+  checkdb,
+  forgetPass,
+  displayList,
+  openHomePage,
+  logoutUser,
+};
--- routes/loginSystemRoute.js
+++ routes/loginSystemRoute.js
@@ -1,16 +1,28 @@
-const express =require('express');
+const express = require("express");
 const router = express.Router();
-const {openLoginSystem, registerUser, openPasswordPage, createPassword, openLoginPage,loginUser, checkdb, forgetPass, displayList, openHomePage, logoutUser} = require('../controllers/controllerLoginSystem');
-router.get('/login_system',openLoginSystem);
-router.post('/action', registerUser);
-router.get('/pass',openPasswordPage);
-router.post('/pass',createPassword);
-router.get('/login',openLoginPage);
-router.post('/login',loginUser);
+const {
+  openLoginSystem,
+  registerUser,
+  openPasswordPage,
+  createPassword,
+  openLoginPage,
+  loginUser,
+  checkdb,
+  forgetPass,
+  displayList,
+  openHomePage,
+  logoutUser,
+} = require("../controllers/controllerLoginSystem");
+router.get("/", openLoginSystem);
+router.post("/action", registerUser);
+router.get("/pass", openPasswordPage);
+router.post("/pass", createPassword);
+router.get("/login", openLoginPage);
+router.post("/login", loginUser);
 router.get("/checkdb", checkdb);
-router.get('/forget_pass',forgetPass);
-router.get('/list', displayList);
-router.get('/home', openHomePage);
-router.get('/logout',logoutUser);
+router.get("/forget_pass", forgetPass);
+router.get("/list", displayList);
+router.get("/home", openHomePage);
+router.get("/logout", logoutUser);
 
-module.exports=router;
+module.exports = router;
